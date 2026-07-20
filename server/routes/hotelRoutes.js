820d5d8b0ea9d9d7112fd3ef656a65d41c5a93b7
Praneeth Kommaluru <pandupraneeth21@gmail.com>
2026-07-20 16:10:58 +0530

    Final Version of Server

--- server/routes/hotelRoutes.js
+++ server/routes/hotelRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
 import { registerHotel } from "../controllers/hotelController.js";
 
+
 const hotelRouter = express.Router();
 
 hotelRouter.post("/", protect, registerHotel);
